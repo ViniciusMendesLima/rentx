4c9d4860d7e3e583edd998acd1774848740a8075
ViniciusMendesLima <viniciusmendeslima@yahoo.com>
2022-04-25 21:30:23 -0300

    erro criação migration

---
+++ src/database/migrations/1650932779330-CreateCategories.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCategories1650932779330 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
